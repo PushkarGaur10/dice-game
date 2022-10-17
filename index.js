9716a93b3f1d8d5d54b4ac834131768eb86a70ec
PushkarGaur10 <115537133+PushkarGaur10@users.noreply.github.com>
2022-10-17 18:52:44 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,61 @@
+function roll1(){
+  var rn1 = Math.floor(Math.random()*6+1);
+  
+  if(rn1 == 1){
+    document.getElementById("img1").src="1.png";
+  } 
+  else
+  if(rn1 == 2){
+    document.getElementById("img1").src="2.jfif";
+  }
+  else
+  if(rn1 == 3){
+    document.getElementById("img1").src="3.jfif";
+  }
+  else
+  if(rn1 == 4){
+    document.getElementById("img1").src="4.png";
+  }
+  else
+  if(rn1 == 5){
+    document.getElementById("img1").src="5.jfif";
+  }
+  else
+  if(rn1 == 6){
+    document.getElementById("img1").src="6.png";
+  }
+
+}
+function roll2(){
+  var rn2 = Math.floor(Math.random()*6+1);
+  
+  if(rn2 == 1){
+    document.getElementById("img2").src="1.png";
+  } 
+  else
+  if(rn2 == 2){
+    document.getElementById("img2").src="2.jfif";
+  }
+  else
+  if(rn2 == 3){
+    document.getElementById("img2").src="3.jfif";
+  }
+  else
+  if(rn2 == 4){
+    document.getElementById("img2").src="4.png";
+  }
+  else
+  if(rn2 == 5){
+    document.getElementById("img2").src="5.jfif";
+  }
+  else
+  if(rn2 == 6){
+    document.getElementById("img2").src="6.png";
+  }
+  
+}
+
+
+
+
+
